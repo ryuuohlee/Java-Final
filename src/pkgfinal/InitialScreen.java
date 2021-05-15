1114f482f6e1e898037fd5bfdd02e1b140fca387
Jeffrey Lee <jglee1@msn.com>
2021-05-15 15:53:46 -0700

    Feature: saving project

---
+++ src/pkgfinal/InitialScreen.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgfinal;
+
+/**
+ *
+ * @author Bassi
+ */
+public class InitialScreen {
+     public static void main(String[] args) {
+          new UserFrame().setVisible(true);
+     }
+    
+}
